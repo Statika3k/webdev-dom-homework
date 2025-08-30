a0d2d6232e37ce49dd10acb8fa01f7cdecc32a39
Nina Shakhanova <shahanova.ns@gmail.com>
2025-08-30 18:14:15 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+import { renderComments } from "./modules/renderComments.js";
+import { sanitizeHtml, formatDate } from "./modules/helpers.js";
+import { comments } from "./modules/commentsInfo.js";
+
+renderComments();
+
+const nameInput = document.querySelector(".add-form-name");
+const textInput = document.querySelector(".add-form-text");
+const addButton = document.querySelector(".add-form-button");
+
+addButton.addEventListener("click", () => {
+  const userName = sanitizeHtml(nameInput.value.trim());
+  const commentText = sanitizeHtml(textInput.value.trim());
+
+  if (userName === "" || commentText === "") {
+    alert("Пожалуйста, заполните все поля");
+    return;
+  }
+
+  // Добавление нового комментария в массив
+  comments.push({
+    userName: userName,
+    date: formatDate(),
+    commentText: commentText,
+    likes: 0,
+    isLiked: false,
+    replies: [],
+  });
+
+  // Перерисовываем комментарии
+  renderComments();
+
+  // Очищаем поля ввода после добавления
+  nameInput.value = "";
+  textInput.value = "";
+});
