f0d8b80618d52d0147a24655aee261c441dfcbc2
Nina Shakhanova <shahanova.ns@gmail.com>
2025-09-10 13:29:15 +0300

    changed formatDate

--- modules/helpers.js
+++ modules/helpers.js
@@ -4,7 +4,7 @@ export const sanitizeHtml = (value) => {
 }
 
 // Получение текущей даты и времени
-export function formatDate() {
+export function formatDate(date) {
     const now = new Date()
     const day = String(now.getDate()).padStart(2, '0')
     const month = String(now.getMonth() + 1).padStart(2, '0')
