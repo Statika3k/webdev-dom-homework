e9cbbcce25c07aa8d27cf3cd1388330ac61084ab
Nina Shakhanova <shahanova.ns@gmail.com>
2025-09-10 13:28:30 +0300

    add GET request

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import { renderComments } from './modules/renderComments.js'
-import {initAddComment} from './modules/initListeners.js'
+import { initAddComment } from './modules/initListeners.js'
+import { updateComments } from './modules/commentsInfo.js'
 
+fetch('https://wedev-api.sky.pro/api/v1/nina-shakhanova/comments', {
+    method: 'GET',
+})
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {        
+        updateComments(data.comments)
+        renderComments()
+    })
 
-renderComments()
 initAddComment(renderComments)
